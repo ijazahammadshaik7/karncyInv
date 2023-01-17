41a72027b7e2decb6979439a3429b0536e696e7c
mohammedsohail422 <mohammedsohail422@gmail.com>
2023-01-17 16:13:23 -0500

    manage funds

--- src/Pages/ManageComponent/ManageGroup.jsx
+++ src/Pages/ManageComponent/ManageGroup.jsx
@@ -8,10 +8,10 @@ const ManageGroup = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
+        <Grid item xs={12} md={6}>
           <KTransaction />
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12} md={6}>
           <DepositTab />
         </Grid>
       </Grid>
